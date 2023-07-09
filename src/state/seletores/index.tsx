309ced413cfecaad5778d74aee9ffe486cf28e17
lealvesac <lealvesac@gmail.com>
2023-07-09 12:48:51 -0300

    finalizaçao-do-projeto

--- src/state/seletores/index.tsx
+++ src/state/seletores/index.tsx
@@ -1,5 +1,6 @@
 import { selector } from "recoil";
 import { filtroDeEventos, listaDeEventosState } from "../atom";
+import { IEvento } from "../../interfaces/IEvento";
 
 export const eventoFiltradoState = selector({
   key: "eventoFiltradoState",
@@ -17,5 +18,18 @@ export const eventoFiltradoState = selector({
     });
 
     return eventos;
+  }
+});
+
+export const eventosAsync = selector({
+  key: "eventosAsync",
+  get: async () => {
+    const respostaHttp = await fetch("http://localhost:8080/eventos")
+    const eventosJson: IEvento[] = await respostaHttp.json()
+    return eventosJson.map(evento => ({
+      ...evento,
+      inicio: new Date(evento.inicio),
+      fim: new Date(evento.fim)
+    }))
   }
 });
